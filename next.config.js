f200ba8fd28c5264a471aa89c068e3b240258b93
Sunil Parekh <sunil@bombinate.com>
2022-06-13 11:37:44 +0100

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    images: {
+      loader: 'akamai',
+      path: '',
+    },  
+  }],
+  
+  nextConfig,
+  // your other plugins here
+
+]);
